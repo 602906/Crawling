ed4fa8c5006f4a5527fca2d2e1c283eaa9a4877e
602906 <3461037726@qq.com>
2026-06-08 23:51:38 +0800

    Delete platforms directory

--- platforms/__init__.py
+++
@@ -1,9 +0,0 @@
-from .kugou import KuGouPlatform
-from .netease import NetEasePlatform
-from .bilibili import BilibiliPlatform
-
-PLATFORMS = {
-    "kugou": KuGouPlatform,
-    "netease": NetEasePlatform,
-    "bilibili": BilibiliPlatform,
-}
