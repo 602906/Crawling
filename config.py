063e318fe599aeb9a6e531349af7f7883b59dbee
602906 <3461037726@qq.com>
2026-06-08 23:53:01 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DOWNLOAD_DIR = os.path.join(BASE_DIR, "downloads")
-HOST = "0.0.0.0"
-PORT = 8000
-
-os.makedirs(DOWNLOAD_DIR, exist_ok=True)
